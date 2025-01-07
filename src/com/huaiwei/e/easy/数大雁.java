ea8c8b6d9c037ef85f01aa437cf77f7262254d85
bx <1831374848@qq.com>
2025-01-07 09:12:00 +0800

    计算三叉树的高度

---
+++ src/com/huaiwei/e/easy/数大雁.java
@@ -0,0 +1,43 @@
+package com.huaiwei.e.easy;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class 数大雁 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String inputSound=sc.nextLine();
+        System.out.println(getDuackNum(inputSound));
+    }
+    //定义一个获取大雁数量的方法
+    public static int getDuackNum(String inputSound){
+        int q=0,u=0,a=0,c=0,k=0;
+        LinkedList<Integer> Q=new LinkedList<>();
+        int cnt []=new int[inputSound.length()+1];
+        int maxNum=0;
+        //便利大雁叫声字符串
+        for (int i=0;i<inputSound.length();i++){
+            char input=inputSound.charAt(i);
+            cnt[i+1]=cnt[i];
+            if (input=='q'){
+                //将当前字符q出现的位置加入到队列中
+                Q.offer(i);
+                q++;
+            }else if (input=='u'&&u<q){
+                u++;
+            }else if(input=='a'&&a<u){
+                a++;
+            }else if (input=='c'&&c<a){
+                c++;
+            } else if (input == 'k'&&k<c) {
+                k++;
+                int first=Q.poll();
+                cnt[i+1]++;
+                maxNum=Math.max(maxNum,cnt[i+1]-cnt[first]);
+            }else if (input !='q' ||input !='u' ||input !='a' ||input !='c' ||input !='k'){
+                return -1;
+            }
+        }
+        return maxNum==0?-1:maxNum;
+    }
+}
---
+++ src/com/huaiwei/e/easy/计算三叉搜索树的高度.java
@@ -0,0 +1,71 @@
+package com.huaiwei.e.easy;
+
+import java.util.Scanner;
+
+public class 计算三叉搜索树的高度 {
+    static class TreeNode {
+        int val;
+        //左子树
+        TreeNode left;
+        //右子树
+        TreeNode right;
+        //中子树
+        TreeNode center;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        sc.nextLine();
+        String nums=sc.nextLine();
+        int maxHigh=1;
+        TreeNode treeNode = new TreeNode(Integer.parseInt(nums.split(" ")[0]));
+        for (int i=1;i<n;i++){
+            int high=calTreeHigh(treeNode,Integer.parseInt(nums.split(" ")[i]));
+            maxHigh=Math.max(maxHigh,high);
+        }
+        System.out.println(maxHigh+1);
+    }
+
+    public static int calTreeHigh(TreeNode tree, int nums) {
+        int high = 1;
+
+        TreeNode treeNode = new TreeNode(nums);
+        while (true) {
+            int diff = nums - tree.val;
+            if (Math.abs(diff) <= 500) {
+
+                if (tree.center == null) {
+                    //如果当前节点的中子树为空，则将新节点的中字数添加到该节点的中子树
+                    tree.center = treeNode;
+                    break;
+                } else {
+                    //如果当前节点的中子树部位空，则将当前节点指向该结点的中子树，下次循环进来的时候该节点中子树不为空，则树高+1
+                    high++;
+                    tree = tree.center;
+                }
+            } else if (diff < -500) {
+            if (tree.left==null){
+                tree.left=treeNode;
+                break;
+            }else {
+                high++;
+                tree=tree.left;
+            }
+            }else {
+             if (tree.right==null){
+                 tree.right=treeNode;
+                 break;
+             }else {
+                 high++;
+                 tree=tree.right;
+             }
+            }
+        }
+        return high;
+    }
+}
